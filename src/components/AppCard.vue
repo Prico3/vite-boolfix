babae7534f10dde376817879da6d14560c75e420
Prico3 <pricoco.andrea@gmail.com>
2022-11-15 16:42:39 +0100

    flags added

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -3,6 +3,16 @@
 export default {
     props: {
         item: Object
+    },
+    data() {
+        return {
+            availableFlags: ['en', 'it']
+        }
+    },
+    methods: {
+        imgUrl(imgName) {
+            return new URL(`../assets/img/${imgName}.png`, import.meta.url).href;
+        }
     }
 }
 </script>
@@ -11,11 +21,18 @@ export default {
     <div>
         <h3>titolo: {{ item.title }}</h3>
         <p>Titolo originale: {{ item.original_title }}</p>
-        <p>Lingua originale: {{ item.original_language }}</p>
+        <div class="language">
+            <img v-if="availableFlags.includes(item.original_language)" :src="imgUrl(item.original_language)" alt="">
+            <p>Lingua originale: {{ item.original_language }}</p>
+        </div>
         <p>Voto: {{ item.vote_average }}</p>
     </div>
 </template>
 
 <style lang="scss" scoped>
-
+.language {
+    img {
+        width: 40px;
+    }
+}
 </style>
